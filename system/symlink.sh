ff561426a85daf7126e877462567c4d6efd6a64c
aravezskinteeth <kelthompson069@gmail.com>
2023-04-20 17:08:14 -0500

    Add half of my config symlinks

--- system/symlink.sh
+++ system/symlink.sh
@@ -5,7 +5,6 @@
 # Source utilities
 #==================================
 . "$HOME/.dotfiles/scripts/utils/utils.sh"
- os_name="$(get_os)"
 
 #==================================
 # Print Section Title
@@ -17,109 +16,96 @@ print_section "Creating Symlinks"
 # Symlink
 #==================================
 
-# fish config
-print_title "Fish configuration"
-symlink ~/.dotfiles/config/fish/config.fish ~/.config/fish/config.fish
-symlink ~/.dotfiles/config/fish/export.fish ~/.config/fish/export.fish
-symlink ~/.dotfiles/config/fish/bindings.fish ~/.config/fish/bindings.fish
-
-symlink ~/.dotfiles/config/fish/functions/arasaka.fish ~/.config/fish/functions/arasaka.fish
-symlink ~/.dotfiles/config/fish/functions/fcd.fish ~/.config/fish/functions/fcd.fish
-symlink ~/.dotfiles/config/fish/functions/flushdns.fish ~/.config/fish/functions/flushdns.fish
-symlink ~/.dotfiles/config/fish/functions/gi.fish ~/.config/fish/functions/gi.fish
-symlink ~/.dotfiles/config/fish/functions/mkcd.fish ~/.config/fish/functions/mkcd.fish
-symlink ~/.dotfiles/config/fish/functions/c_prettyping.fish ~/.config/fish/functions/c_prettyping.fish
-symlink ~/.dotfiles/config/fish/functions/c_mtr.fish ~/.config/fish/functions/c_mtr.fish
-symlink ~/.dotfiles/config/fish/functions/ql.fish ~/.config/fish/functions/ql.fish
-symlink ~/.dotfiles/config/fish/functions/sreload.fish ~/.config/fish/functions/sreload.fish
-symlink ~/.dotfiles/config/fish/functions/treload.fish ~/.config/fish/functions/treload.fish
-symlink ~/.dotfiles/config/fish/functions/supdate.fish ~/.config/fish/functions/supdate.fish
-symlink ~/.dotfiles/config/fish/functions/pupdate.fish ~/.config/fish/functions/pupdate.fish
-
-symlink ~/.dotfiles/config/fish/theme/excalith.fish ~/.config/fish/theme/excalith.fish
-touch ~/.config/fish/local.fish
-
 # bash config
 print_title "Bash configuration"
 symlink ~/.dotfiles/config/bash/.bashrc ~/.bashrc
-symlink ~/.dotfiles/config/bash/aliases.bash ~/.config/bash/aliases.bash
-symlink ~/.dotfiles/config/bash/functions.bash ~/.config/bash/functions.bash
-touch ~/.bash.local
 
 # zsh config
 print_title "Zsh configuration"
 symlink ~/.dotfiles/config/zsh/.zshrc ~/.zshrc
-touch ~/.zsh.local
+symlink ~/.dotfiles/config/zsh/.zshenv ~/.zshenv
+symlink ~/.dotfiles/config/zsh/.zsh ~/.zsh
+symlink ~/.dotfiles/config/zsh/.zprofile ~/.zprofile
+symlink ~/.dotfiles/config/zsh/.profile ~/.profile
 
 # starship config
 print_title "Starship configuration"
 symlink ~/.dotfiles/config/starship/starship.toml ~/.config/starship.toml
 
+# kitty config
+print_title "Kitty configuration"
+symlink ~/.dotfiles/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+symlink ~/.dotfiles/config/kitty/diff.conf ~/.config/kitty/diff.conf
+symlink ~/.dotfiles/config/kitty/themes/ ~/.config/kitty/themes/
+
 # git config
 print_title "Git configuration"
-symlink ~/.dotfiles/config/git/config ~/.config/git/config
+symlink ~/.dotfiles/config/git/.gitconfig ~/.config/git/.gitconfig
 symlink ~/.dotfiles/config/git/ignore_global ~/.config/git/ignore_global
-touch ~/.config/git/config.local
+
+# nvim config
+print_title "Neovim configuration"
+symlink ~/.dotfiles/config/astronvim-config # ~/.config/nvim/lua/user
+symlink ~/.dotfiles/neovim/nvchad-config # ~/.config/NvChad/lua/custom
+symlink ~/.dotfiles/neovim/nvim-custom # ~/.config/nvim-custom
+
+# yabai config
+print_title "Yabai configuration"
+symlink ~/.dotfiles/config/yabai/.yabairc ~/.config/yabai/yabairc
+
+# skhd config
+print_title "SKHD configuration"
+symlink ~/.dotfiles/config/skhd/.skhdrc ~/.config/skhd/skhdrc
 
 # neofetch config
 print_title "Neofetch configuration"
 symlink ~/.dotfiles/config/neofetch/config.conf ~/.config/neofetch/config.conf
 
-# fastfatch config
-print_title "Fastfatch configuration"
-symlink ~/.dotfiles/config/fastfatch/config.conf ~/.config/fastfatch/config.conf
-
 # ranger commander theme
-print_title "Ranger configuration"
-symlink ~/.dotfiles/config/ranger/rc.conf ~/.config/ranger/rc.conf
+# print_title "Ranger configuration"
+# symlink ~/.dotfiles/config/ranger/rc.conf ~/.config/ranger/rc.conf
 
-# midnight commander theme
-print_title "Midnight Commander configuration"
-symlink ~/.dotfiles/config/mc/ini ~/.config/mc/ini
-symlink ~/.dotfiles/config/mc/skins/Arasaka.ini ~/.local/share/mc/skins/Arasaka.ini 
+# bat config & themes
+print_title "Bat configuration"
+symlink ~/.dotfiles/config/bat/config ~/.config/bat/config
+symlink ~/.dotfiles/config/bat/themes/ ~/.config/bat/themes/
 
-# alacritty config
-print_title "Alacritty configuration"
-if [ "$os_name" == "macos" ]; then
-	symlink ~/.dotfiles/config/alacritty/alacrittyMacos.yml ~/.config/alacritty/alacritty.yml
-elif [ "$os_name" == "ubuntu" ]; then
-	symlink ~/.dotfiles/config/alacritty/alacrittyLinux.yml ~/.config/alacritty/alacritty.yml
-fi
+# bpytop config
+print_title "Bpytop configuration"
+symlink ~/.dotfiles/config/bpytop/ ~/.config/bpytop/
 
-# kitty config
-# print_title "Kitty configuration"
-# symlink ~/.dotfiles/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
-# if [ "$os_name" == "macos" ]; then
-# 	symlink ~/.dotfiles/config/kitty/keymapMacos.conf ~/.config/kitty/keymap.conf
-# elif [ "$os_name" == "ubuntu" ]; then
-# 	symlink ~/.dotfiles/config/kitty/keymapLinux.conf ~/.config/kitty/keymap.conf
-# fi
-# symlink ~/.dotfiles/config/kitty/openactions.conf ~/.config/kitty/openactions.conf
-# symlink ~/.dotfiles/config/kitty/theme.conf ~/.config/kitty/theme.conf
+# btop config
+print_title "Btop configuration"
+symlink ~/.dotfiles/config/btop/ ~/.config/btop/
 
-# tmux config
-print_title "tmux configuration"
-symlink ~/.dotfiles/config/tmux/tmux.conf ~/.config/tmux/tmux.conf
+# calcure config: Modern YUI calendar & task manager
+print_title "Calcure configuration"
+symlink ~/.dotfiles/config/calcure/ ~/.config/calcure/
 
-# lunar vim config
-print_title "LunarVim configuration"
-symlink ~/.dotfiles/config/lvim/config.lua ~/.config/lvim/config.lua
+# cava config
+print_title "Cava configuration"
+symlink ~/.dotfiles/config/cava/ ~/.config/cava/
 
-if [ "$os_name" == "macos" ]; then
-	# yabai config
-	print_title "Yabai configuration"
-	symlink ~/.dotfiles/config/yabai/.yabairc ~/.config/yabairc
+# emacs config
+print_title "Doom Emacs configuration"
+symlink ~/.dotfiles/config/emacs/.doom.d/ ~/.doom.d/
 
-	# skhd config
-	print_title "SKHD configuration"
-	symlink ~/.dotfiles/config/skhd/.skhdrc ~/.config/skhdrc
+# gh config
+print_title "Gh configuration"
+symlink ~/.dotfiles/config/gh/ ~/.config/gh/
 
-	# karabiner config
-	print_title "Karabiner configuration"
-	symlink ~/.dotfiles/config/karabiner/karabiner.json ~/.config/karabiner/karabiner.json
-	symlink ~/.dotfiles/config/karabiner/complex_modifications.json ~/.config/karabiner/assets/complex_modifications/complex_modifications.json
-elif [ "$os_name" == "ubuntu" ]; then
-	# bat configuration
-	print_title "bat binary"
-	symlink /usr/bin/batcat ~/.local/bin/bat
-fi
+# hyfetch config
+print_title "Hyfetch configuration"
+symlink ~/.dotfiles/config/hyfetch/hyfetch.json ~/.config/hyfetch.json
+
+# lazygit config
+print_title "Lazy git configuration"
+symlink ~/.dotfiles/config/lazygit/ ~/.config/lazygit/
+
+# lsd config
+print_title "LSD configuration"
+symlink ~/.dotfiles/config/lsd/ ~/.config/lsd/
+
+# mpv config
+print_title "MPV configuration"
+symlink ~/.dotfiles/config/mpv/ ~/.config/mpv/
