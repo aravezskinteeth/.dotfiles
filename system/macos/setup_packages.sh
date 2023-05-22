cd64ce90e07c268d6ec01d9db77aacba85bfb6d6
aravezskinteeth <kelthompson069@gmail.com>
2023-05-22 14:11:16 -0500

    comment miniconda instalation

--- system/macos/setup_packages.sh
+++ system/macos/setup_packages.sh
@@ -235,10 +235,10 @@ execute "sudo tlmgr install latexmk"
 print_title "Install Packages From Source"
 
 # MiniConda
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
+# mkdir -p ~/miniconda3
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda3/miniconda.sh
+# bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+# rm -rf ~/miniconda3/miniconda.sh
 # ~/miniconda3/bin/conda init zsh
 
 # fm file manager
