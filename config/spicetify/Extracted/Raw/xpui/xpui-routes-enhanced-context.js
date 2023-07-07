eab7c2a61a9b13a4805d5ae693690ed5e80ac34a
aravezskinteeth <kelthompson069@gmail.com>
2023-07-07 10:36:27 -0500

    Update spicetify config

--- config/spicetify/Extracted/Raw/xpui/xpui-routes-enhanced-context.js
+++ config/spicetify/Extracted/Raw/xpui/xpui-routes-enhanced-context.js
@@ -1,1 +1,1 @@
-"use strict";(("undefined"!=typeof self?self:global).webpackChunkopen=("undefined"!=typeof self?self:global).webpackChunkopen||[]).push([[7405,682],{7808:(e,l,s)=>{s.d(l,{s:()=>f.s});var f=s(82930)}}]);
+"use strict";(("undefined"!=typeof self?self:global).webpackChunkopen=("undefined"!=typeof self?self:global).webpackChunkopen||[]).push([[7405,682],{9284:(e,l,s)=>{s.d(l,{s:()=>f.s});var f=s(3882)}}]);
