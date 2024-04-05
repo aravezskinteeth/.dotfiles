e9df33689ccb7e017c813c2ad11670ae9803c185
livisupajares <livisupajares@gmail.com>
2024-04-04 22:52:56 -0500

    Create fish config file

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,3 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
-# Source aliases
-source ~/.dotfiles/config/fish/aliases.fish
-source ~/.dotfiles/config/shell/.aliases
-
-# Setup Starship (at the end)
-starship init fish | source
