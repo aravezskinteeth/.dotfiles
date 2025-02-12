0410cc1fe50ba9e65ca71ac3dcfa6246d5ab3f13
Livisu Pajares <livisupajares@gmail.com>
2025-02-11 20:14:41 -0500

    Add symlink in statusbar

--- config/yazi/init.lua
+++ config/yazi/init.lua
@@ -4,3 +4,13 @@ require("full-border"):setup({
 	type = ui.Border.ROUNDED,
 })
 
+-- Add symlink in statusbar
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
+end, 3300, Status.LEFT)
+
