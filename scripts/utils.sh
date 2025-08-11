7b3e25c69822f69b324ef69f87812715415cc099
Livisu Pajares <livisupajares@gmail.com>
2025-08-10 21:38:21 -0500

    feat(utils): Add function to print main sections
    - Takes one or more arguments as a section title.
    - Wraps the title in an ASCII art box with rounded corners.
    - Prints it in green (from another function).
    - Adds line breaks before and after, for spacing.

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -9,6 +9,42 @@ symlink() {
 #==================================
 # Print
 #==================================
+print_section() {
+  local TITLE="$*"
+  local TITLE_LENGTH=${#TITLE}
+  local BORDER_LENGTH=$((TITLE_LENGTH + 18))
+
+  local i
+  local BANNER_TOP
+  for ((i = 0; i < BORDER_LENGTH; ++i)); do
+    if [ $i = 0 ]; then
+      BANNER_TOP+="╭"
+    elif [ $i = $(($BORDER_LENGTH - 1)) ]; then
+      BANNER_TOP+="╮"
+    else
+      BANNER_TOP+="─"
+    fi
+  done
+
+  local BANNER_BOTTOM
+  for ((i = 0; i < BORDER_LENGTH; ++i)); do
+    if [ $i = 0 ]; then
+      BANNER_BOTTOM+="╰"
+    elif [ $i = $(($BORDER_LENGTH - 1)) ]; then
+      BANNER_BOTTOM+="╯"
+    else
+      BANNER_BOTTOM+="─"
+    fi
+  done
+
+  print_linke_break
+  print_in_green "$BANNER_TOP"
+  print_in_green "\n│        $TITLE        │\n"
+  print_in_green "$BANNER_BOTTOM"
+  print_linke_break
+
+}
+
 print_title() {
   print_in_color "\n • $1\n" 5
 }
